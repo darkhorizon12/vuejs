f16ac2afa30b3ab6bc843f230be495cedbc282fd
fenrir <finrir@matey.co.kr>
2023-02-14 10:07:15 +0900

    completed coaches list

--- main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
+++ main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
@@ -27,7 +27,7 @@ export default {
       return this.$route.path + "/" + this.id + "/contact";
     },
     coachDetailsLink() {
-      return this.$route.path + "/ " + this.id;
+      return this.$route.path + "/" + this.id;
     }
   }
 }
